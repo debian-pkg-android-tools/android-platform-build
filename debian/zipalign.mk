2ea84375869a5d70d8de1047ff37568b93be8422
Kai-Chung Yan <seamlikok@gmail.com>
2015-08-18 16:06:15 +0800

    d/zipalign.mk: Add missing library linking flags

--- debian/zipalign.mk
+++ debian/zipalign.mk
@@ -2,9 +2,9 @@ include /usr/include/android/arch/android_includes.mk
 
 NAME = zipalign
 SOURCES = ZipAlign.cpp ZipEntry.cpp ZipFile.cpp
-CPPFLAGS += $(ANDROID_INCLUDES) -I../../core/combp/include
-LDFLAGS += -fPIC -Wl,-rpath=/usr/lib/android -lpthread -lrt \
-           -L/usr/lib/android -lutils -lcutils -llog
+CPPFLAGS += $(ANDROID_INCLUDES) -I/usr/include/android -I../../core/combp/include
+LDFLAGS += -fPIC -Wl,-rpath=/usr/lib/android -lpthread -lrt -lzopfli -lz \
+           -L/usr/lib/android -lutils -lcutils -llog -landroidfw
 
 build: $(SOURCES)
 	c++ $^ -o $(NAME) $(CXXFLAGS) $(CPPFLAGS) $(LDFLAGS)
