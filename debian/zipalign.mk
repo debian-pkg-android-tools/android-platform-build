b9445862fbdae3f68cb0fb845ed637de3596bc68
Kai-Chung Yan <seamlikok@gmail.com>
2015-11-27 18:50:35 +0800

    Refactor makefile to build on top-level directory

--- debian/zipalign.mk
+++ debian/zipalign.mk
@@ -1,12 +1,13 @@
 NAME = zipalign
 SOURCES = ZipAlign.cpp ZipEntry.cpp ZipFile.cpp
-CPPFLAGS += -I/usr/include/android
+SOURCES := $(foreach source, $(SOURCES), tools/zipalign/$(source))
+CPPFLAGS += -I/usr/include/android \
             -include core/combo/include/arch/linux-$(CPU)/AndroidConfig.h
 LDFLAGS += -fPIC -Wl,-rpath=/usr/lib/android -lpthread -lrt -lzopfli -lz \
            -L/usr/lib/android -lutils -lcutils -llog -landroidfw
 
 build: $(SOURCES)
-	c++ $^ -o $(NAME) $(CXXFLAGS) $(CPPFLAGS) $(LDFLAGS)
+	$(CXX) $^ -o $(NAME) $(CXXFLAGS) $(CPPFLAGS) $(LDFLAGS)
 
 clean:
-	rm -f $(NAME)
+	$(RM) $(NAME)
