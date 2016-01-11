6a9003308734570613f02142ee4aaee5721945da
Kai-Chung Yan <seamlikok@gmail.com>
2016-01-11 09:04:51 +0800

    No need librt or libcutils

--- debian/zipalign.mk
+++ debian/zipalign.mk
@@ -2,8 +2,8 @@ NAME = zipalign
 SOURCES = ZipAlign.cpp ZipEntry.cpp ZipFile.cpp
 SOURCES := $(foreach source, $(SOURCES), tools/zipalign/$(source))
 CPPFLAGS += -I/usr/include/android -include android/arch/AndroidConfig.h
-LDFLAGS += -fPIC -Wl,-rpath=/usr/lib/android -lpthread -lrt -lzopfli -lz \
-           -L/usr/lib/android -lutils -lcutils -llog -landroidfw
+LDFLAGS += -fPIC -Wl,-rpath=/usr/lib/android -lpthread -lzopfli -lz \
+           -L/usr/lib/android -lutils -llog -landroidfw
 
 build: $(SOURCES)
 	$(CXX) $^ -o $(NAME) $(CXXFLAGS) $(CPPFLAGS) $(LDFLAGS)
