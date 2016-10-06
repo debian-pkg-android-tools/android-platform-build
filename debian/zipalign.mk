97cc2385ac2fd574b7846b20355bb4f1dff195d3
Kai-Chung Yan (殷啟聰) <seamlikok@gmail.com>
2016-10-06 14:44:52 +0800

    Clean up build scripts

--- debian/zipalign.mk
+++ debian/zipalign.mk
@@ -1,12 +1,10 @@
 NAME = zipalign
 SOURCES = ZipAlign.cpp ZipEntry.cpp ZipFile.cpp
 SOURCES := $(foreach source, $(SOURCES), tools/zipalign/$(source))
-CPPFLAGS += -I/usr/include/android \
-            -include core/combo/include/arch/linux-x86/AndroidConfig.h
-LDFLAGS += -fPIC \
-           -Wl,-rpath=/usr/lib/$(DEB_HOST_MULTIARCH)/android:/usr/lib/android \
+CPPFLAGS += -include core/combo/include/arch/linux-x86/AndroidConfig.h
+LDFLAGS += -Wl,-rpath=/usr/lib/$(DEB_HOST_MULTIARCH)/android \
            -lpthread -lzopfli -lz \
-           -L/usr/lib/$(DEB_HOST_MULTIARCH)/android -L/usr/lib/android \
+           -L/usr/lib/$(DEB_HOST_MULTIARCH)/android \
            -lutils -llog -landroidfw
 
 build: $(SOURCES)
