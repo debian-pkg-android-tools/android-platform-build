c600992a2d2956ad735d44f00dc192e6ecfca206
Kai-Chung Yan (殷啟聰) <seamlikok@gmail.com>
2016-10-06 15:20:20 +0800

    Refresh build scripts

--- debian/ziptime.mk
+++ debian/ziptime.mk
@@ -1,10 +1,6 @@
 NAME = ziptime
 SOURCES = ZipTime.cpp ZipEntry.cpp ZipFile.cpp
 SOURCES := $(foreach source, $(SOURCES), tools/ziptime/$(source))
-LDFLAGS += -Wl,-rpath=/usr/lib/$(DEB_HOST_MULTIARCH)/android \
-           -lpthread -lzopfli -lz \
-           -L/usr/lib/$(DEB_HOST_MULTIARCH)/android \
-           -lutils -llog -landroidfw
 
 build: $(SOURCES)
 	$(CXX) $^ -o $(NAME) $(CXXFLAGS) $(CPPFLAGS) $(LDFLAGS)
