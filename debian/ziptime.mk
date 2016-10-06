0b7a382188cbdca80f710ec65684257caecd04cb
Kai-Chung Yan (殷啟聰) <seamlikok@gmail.com>
2016-10-06 14:58:24 +0800

    No longer provide AndroidConfig.h

--- debian/ziptime.mk
+++ debian/ziptime.mk
@@ -1,7 +1,6 @@
 NAME = ziptime
 SOURCES = ZipTime.cpp ZipEntry.cpp ZipFile.cpp
 SOURCES := $(foreach source, $(SOURCES), tools/ziptime/$(source))
-CPPFLAGS += -include core/combo/include/arch/linux-x86/AndroidConfig.h
 LDFLAGS += -Wl,-rpath=/usr/lib/$(DEB_HOST_MULTIARCH)/android \
            -lpthread -lzopfli -lz \
            -L/usr/lib/$(DEB_HOST_MULTIARCH)/android \
