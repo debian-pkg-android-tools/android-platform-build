efc9c8d824fc8ae2c03abf7c222fd222b4b407d2
Hans-Christoph Steiner <hans@eds.org>
2016-10-10 19:37:16 +0200

    remove unnecessary linked libs from ziptime

--- debian/ziptime.mk
+++ debian/ziptime.mk
@@ -3,9 +3,8 @@ SOURCES = ZipTime.cpp ZipEntry.cpp ZipFile.cpp
 SOURCES := $(foreach source, $(SOURCES), tools/ziptime/$(source))
 CPPFLAGS += -include core/combo/include/arch/linux-x86/AndroidConfig.h
 LDFLAGS += -Wl,-rpath=/usr/lib/$(DEB_HOST_MULTIARCH)/android \
-           -lpthread -lzopfli -lz \
-           -L/usr/lib/$(DEB_HOST_MULTIARCH)/android \
-           -lutils -llog -landroidfw
+           -lpthread \
+           -L/usr/lib/$(DEB_HOST_MULTIARCH)/android
 
 build: $(SOURCES)
 	$(CXX) $^ -o $(NAME) $(CXXFLAGS) $(CPPFLAGS) $(LDFLAGS)
